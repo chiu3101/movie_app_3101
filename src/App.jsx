1a5e34641464d64dc5c424c511bd52c0d5df186f
Amruta Tandale <Amruta Tandale@tcognition.com>
2026-03-02 13:13:01 +0530

    Search with Debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useDebounce} from 'react-use';
 import { useState, useEffect } from "react";
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
@@ -21,13 +22,26 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
+  //Debounce the search term to prevent making too many API requests
+  //by waiting for a specified delay (500ms in this case) after the user stops typing before updating the debounced search term. This helps to reduce the number of API calls and improve performance, especially when the user is typing quickly.
+  useDebounce(
+    () => 
+      setDebouncedSearchTerm(searchTerm),
+      500, // Debounce delay of 500ms
+    [searchTerm]
+  );
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setLoading(true);
     setErrorMessage('');
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = debouncedSearchTerm 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(debouncedSearchTerm)}`
+      //encodeURIComponent is used to ensure that any special characters in the search term are properly encoded for use in a URL. This prevents issues with spaces and other characters that may not be valid in a URL.
+      //shortly used so that the query is passed properly to the API endpoint, allowing for accurate search results based on the user's input. If the query is empty, it defaults to fetching popular movies.
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error("Failed to fetch movies");
@@ -51,8 +65,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);//when the component mounts, it will fetch movies based on the debounced search term. If the debounced search term is empty, it will fetch popular movies.
+  }, [debouncedSearchTerm]);//this effect runs whenever the debouncedSearchTerm changes, allowing the app to fetch new movies based on the user's input.
 
   return (
     <main>
